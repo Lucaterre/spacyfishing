3c68d3712e604b282694bca37b1c3ab203749297
Lucaterre <ls.terriel@gmail.com>
2022-06-15 14:25:42 +0200

    updated tests + documentation + add new span attributes + add new components parameters + clarify HTTP exceptions response

--- spacyfishing/entity_fishing_linker.py
+++ spacyfishing/entity_fishing_linker.py
@@ -1,13 +1,12 @@
 # -*- coding: UTF-8 -*-
 
-"""
-entity_fishing_linker.py
+"""entity_fishing_linker.py
 
 SpaCy wrapper to call Entity-fishing API
 as disambiguation and entity linking component.
 """
 
-import time
+import logging
 
 import requests
 
@@ -18,7 +17,9 @@ from spacy.tokens import Doc, Span
 @Language.factory("entityfishing", default_config={
     "api_ef_base": "http://nerd.huma-num.fr/nerd/service",
     "language": "en",
-    "description_required": False
+    "extra_info": False,
+    "filter_statements": [],
+    "verbose": False
 })
 class EntityFishing:
     """EntityFishing component for spaCy pipeline."""
@@ -27,30 +28,43 @@ class EntityFishing:
                  name: str,
                  api_ef_base: str,
                  language: str,
-                 description_required: bool):
+                 extra_info: bool,
+                 filter_statements: list,
+                 verbose: bool):
         """
-        Show default config for default attributes values.
+        `EntityFishing` main class component.
+
+        Note:
+            Show default config for default attributes values.
 
         Parameters:
             api_ef_base (str): describes url of the entity-fishing API used.
             language (str): matches the language of the resources to
             be disambiguated (matches the language model for the NER task).
-            description_required (bool): flag to search or not the description
-            associated with the Wikidata entity.
+            extra_info (bool): attach extra informations to spans as normalised term,
+            description, others knowledge base ids.
+            filter_statements (list): filter others KB ids
+            that relies on QID  eg. ['P214', 'P244'].
+            verbose (bool): display logging messages.
 
         Attributes:
             api_ef_base (str): cf. `api_ef_base` in parameters section.
             language (dict): cf. `language` in parameters section.
             prepare the language argument for the query.
-            flag_desc (str): cf. `description_required` in parameters section.
             wikidata_url_base (str): wikidata base url (to concatenate QID identifiers).
+            flag_extra (bool): cf. `extra_info` in parameters section.
+            filter_statements (list): cf. `filter_statements` in parameters section.
+            verbose (bool): cf. `verbose` in parameters section.
         """
         if not api_ef_base.endswith("/"):
             api_ef_base += "/"
         self.api_ef_base = api_ef_base
         self.language = dict(lang=language)
-        self.flag_desc = description_required
         self.wikidata_url_base = "https://www.wikidata.org/wiki/"
+
+        self.flag_extra = extra_info
+        self.filter_statements = filter_statements
+        self.verbose = verbose
 
         # Set doc extensions to attaches raw response from Entity-Fishing API to doc
         Doc.set_extension("annotations", default=None, force=True)
@@ -58,24 +72,42 @@ class EntityFishing:
 
         # Set spans extensions to enhance spans with new information
         # come from Wikidata knowledge base.
+        # default spans :
         Span.set_extension("kb_qid", default=None, force=True)
-        Span.set_extension("description", default=None, force=True)
+        Span.set_extension("wikipedia_page_ref", default=None, force=True)
         Span.set_extension("url_wikidata", default=None, force=True)
         Span.set_extension("nerd_score", default=None, force=True)
 
+        # spans if extra_info set to True
+        Span.set_extension("normal_term", default=None, force=True)
+        Span.set_extension("description", default=None, force=True)
+        Span.set_extension("src_description", default=None, force=True)
+        Span.set_extension("other_ids", default=None, force=True)
+
     @staticmethod
-    def generic_client(method: str, url: str, params: dict = {}, files: dict = {}) -> requests.Response:
+    def generic_client(method: str,
+                       url: str,
+                       verbose: bool,
+                       params: dict = None,
+                       files: dict = None) -> requests.Response:
         """Client to interact with a generic Rest API.
 
         Parameters:
             method (str): service HTTP methods (get, post etc.)
             url (str): the base URL to the service being used.
+            verbose (bool): display log messages.
             params (dict): requests parameters.
             files (dict): requests files.
 
         Returns:
             response (requests.Response): query response.
         """
+
+        if params is None:
+            params = {}
+        if files is None:
+            files = {}
+
         def make_requests(type_method: str,
                           type_url: str,
                           type_params: dict,
@@ -89,27 +121,40 @@ class EntityFishing:
                                    params=type_params)
             return res
 
+        def client_log(msg: str) -> None:
+            if verbose:
+                logging.warning(msg)
+
         response = make_requests(method, url, params, files)
 
-        # Retry request if too many requests
-        if response.status_code == 429:
-            time.sleep(int(response.headers["Retry-After"]))
-            response = make_requests(method, url, params, files)
+        # Manage response status code :
+        # cf. https://nerd.readthedocs.io/en/latest/restAPI.html#response-status-codes
+        if response.status_code == 400:
+            client_log("Wrong request, missing parameters, "
+                       "missing header, text too short (> 5 chars). (400)")
+        elif response.status_code == 500:
+            client_log("Entity-Fishing API service seems broken. (500)")
+        elif response.status_code == 404:
+            client_log("Property was not found in request body. (404)")
+        elif response.status_code == 406:
+            client_log("Language is not supported by Entity-Fishing. (406)")
 
         return response
 
-    def concept_look_up(self, kb_qid: str) -> requests.Response:
-        """Service returns the knowledge base concept information from QID.
+    def concept_look_up(self, wiki_id: str) -> requests.Response:
+        """Service returns the knowledge base concept information from QID
+        or Wikipedia page id.
 
         Parameters:
-            kb_qid (str): Wikidata identifier (QID)
+            wiki_id (str): Wikidata identifier (QID) or Wikipedia page external reference
         Returns:
             response (requests.Response): query response from concept look-up service.
         """
-        url_concept_lookup = self.api_ef_base + "kb/concept/" + kb_qid
+        url_concept_lookup = self.api_ef_base + "kb/concept/" + wiki_id
         return self.generic_client(method="GET",
                                    url=url_concept_lookup,
-                                   params=self.language)
+                                   params=self.language,
+                                   verbose=self.verbose)
 
     def disambiguate_text(self, files: dict) -> requests.Response:
         """Service returns disambiguate entities.
@@ -121,7 +166,10 @@ class EntityFishing:
             response (requests.Response): query response from concept look-up service.
         """
         url_disambiguate = self.api_ef_base + "disambiguate"
-        return self.generic_client(method='POST', url=url_disambiguate, files=files)
+        return self.generic_client(method='POST',
+                                   url=url_disambiguate,
+                                   files=files,
+                                   verbose=self.verbose)
 
     def __call__(self, doc: Doc) -> Doc:
         """Attaches entities to spans (and doc)."""
@@ -142,7 +190,6 @@ class EntityFishing:
         # Post query to Entity-Fishing disambiguation service
         req = self.disambiguate_text(files=data)
 
-        #req.raise_for_status()
         try:
             res = req.json()
         except requests.exceptions.JSONDecodeError:
@@ -157,23 +204,64 @@ class EntityFishing:
             "encoding": req.encoding
         }
 
-        # Attach wikidata QID, wikidata url, description (optional) and ranking disambiguation score
-        if len(res['entities']) > 0 and len(doc.ents) > 0 and req.status_code == 200:
-            for entity in res['entities']:
-                try:
-                    span = doc[entity['offsetStart']:entity['offsetEnd']]
-                    span._.kb_qid = entity['wikidataId']
-                    span._.url_wikidata = self.wikidata_url_base + span._.kb_qid
-                    span._.nerd_score = entity['nerd_selection_score']
-                    if self.flag_desc:
+        # Attach to span default information: wikidata QID,
+        # wikidata url and ranking disambiguation score
+        if res != {} and len(doc.ents) > 0 and req.status_code == 200:
+            try:
+                for entity in res['entities']:
+                    try:
+                        span = doc[entity['offsetStart']:entity['offsetEnd']]
+                        span._.kb_qid = str(entity['wikidataId'])
                         try:
-                            req_desc = self.concept_look_up(span._.kb_qid)
-                            req_desc.raise_for_status()
-                            res_desc = req_desc.json()
-                            span._.description = res_desc['definitions'][0]['definition']
+                            span._.wikipedia_page_ref = str(entity["wikipediaExternalRef"])
                         except KeyError:
                             pass
-                except KeyError:
-                    pass
+                        span._.url_wikidata = self.wikidata_url_base + span._.kb_qid
+                        span._.nerd_score = entity['nerd_selection_score']
+                        # Attach to span extra information:  normalised term name, description,
+                        # description source, others identifiers (statements attach to QID)
+                        if self.flag_extra and span._.wikipedia_page_ref is not None:
+                            try:
+                                req_desc = self.concept_look_up(span._.wikipedia_page_ref)
+                                res_desc = req_desc.json()
+                                # normalised term name
+                                try:
+                                    span._.normal_term = res_desc['preferredTerm']
+                                except KeyError:
+                                    pass
+                                # description and source description (filter by language)
+                                try:
+                                    span._.description = res_desc['definitions'][0]["definition"]
+                                    span._.src_description = res_desc['definitions'][0]["source"]
+                                except KeyError:
+                                    pass
+                                # others identifiers attach to QID
+                                # in Wikidata KB with filter properties or not
+                                try:
+                                    if len(self.filter_statements) != 0:
+                                        ids = [
+                                            {
+                                                k: content[k] for k in ['propertyName',
+                                                                        'propertyId',
+                                                                        'value']
+                                             } for content in res_desc['statements']
+                                            if content['propertyId'] in self.filter_statements
+                                        ]
+                                    else:
+                                        ids = [
+                                            {
+                                                k: content[k] for k in ['propertyName',
+                                                                        'propertyId',
+                                                                        'value']
+                                             } for content in res_desc['statements']]
+                                    span._.other_ids = ids
+                                except KeyError:
+                                    pass
+                            except requests.exceptions.JSONDecodeError:
+                                pass
+                    except KeyError:
+                        pass
+            except KeyError:
+                pass
 
         return doc
--- tests/test_EF_component.py
+++ tests/test_EF_component.py
@@ -6,44 +6,71 @@ import spacy
 
 import spacyfishing
 
-class TestEfSpacy(unittest.TestCase):
+
+class TestEfSpacyNormalConfig(unittest.TestCase):
     @classmethod
     def setUpClass(cls) -> None:
-        nlp = spacy.load("en_core_web_sm")
-        nlp.add_pipe("entityfishing", after="ner")
-        cls.doc = nlp("Austria invaded and fought the Serbian army at the Battle of Cer and Battle of Kolubara beginning on 12 August. "
-                      "\n\nThe army, led by general Paul von Hindenburg defeated Russia in a series of "
-                      "battles collectively known as the First Battle of Tannenberg (17 August – 2 September). "
-                      "But the failed Russian invasion, causing the fresh German troops to move to the east, "
-                      "allowed the tactical Allied victory at the First Battle of the Marne. "
-                      "\n\nUnfortunately for the Allies, the pro-German King Constantine "
-                      "I dismissed the pro-Allied government of E. Venizelos before "
-                      "the Allied expeditionary force could arrive. Beginning in 1915, "
-                      "the Italians under Cadorna mounted eleven offensives on the Isonzo "
-                      "front along the Isonzo River, northeast of Trieste.\n\n At the Siege of Maubeuge "
-                      "about 40000 French soldiers surrendered, at the battle of Galicia Russians took about 100-120000 "
-                      "Austrian captives, at the Brusilov Offensive about 325 000 to 417 000 Germans and Austrians surrendered "
-                      "to Russians, at the Battle of Tannenberg 92,000 Russians surrendered.\n\n After marching through Belgium, "
-                      "Luxembourg and the Ardennes, the German Army advanced, in the latter half of August, into northern France "
-                      "where they met both the French army, under Joseph Joffre, and the initial six divisions of the British "
-                      "Expeditionary Force, under Sir John French. A series of engagements known as the Battle of the Frontiers ensued. "
-                      "Key battles included the Battle of Charleroi and the Battle of Mons. In the former battle the French 5th Army "
-                      "was almost destroyed by the German 2nd and 3rd Armies and the latter delayed the German advance by a day. A general "
-                      "Allied retreat followed, resulting in more clashes such as the Battle of Le Cateau, "
-                      "the Siege of Maubeuge and the Battle of St. Quentin (Guise). \n\nThe German army came within 70 km (43 mi) of Paris, "
-                      "but at the First Battle of the Marne (6–12 September), French and British troops were able to force a German retreat "
-                      "by exploiting a gap which appeared between the 1st and 2nd Armies, ending the German advance into France. "
-                      "The German army retreated north of the Aisne River and dug in there, establishing the beginnings of a static western "
-                      "front that was to last for the next three years. Following this German setback, the opposing forces tried to "
-                      "outflank each other in the Race for the Sea, and quickly extended their trench systems from the North Sea to the Swiss frontier. "
-                      "The resulting German-occupied territory held 64% of France's pig-iron production, 24% of its steel manufacturing, dealing a serious, "
-                      "but not crippling setback to French industry.\n ")
+        cls.text_example = """
+        Austria invaded and fought the Serbian army at the Battle of Cer and 
+        Battle of Kolubara beginning on 12 August. \n\nThe army, led by general Paul von Hindenburg 
+        defeated Russia in a series of battles collectively known as the First Battle of Tannenberg 
+        (17 August – 2 September). But the failed Russian invasion, causing the fresh German troops to move to the east, 
+        allowed the tactical Allied victory at the First Battle of the Marne. \n\nUnfortunately for the Allies, 
+        the pro-German King Constantine I dismissed the pro-Allied government of E. Venizelos before 
+        the Allied expeditionary force could arrive. Beginning in 1915, the Italians under 
+        Cadorna mounted eleven offensives on the Isonzo front along the Isonzo River, 
+        northeast of Trieste.\n\n At the Siege of Maubeuge about 40000 French soldiers surrendered, 
+        at the battle of Galicia Russians took about 100-120000 Austrian captives, 
+        at the Brusilov Offensive about 325 000 to 417 000 Germans and Austrians 
+        surrendered to Russians, at the Battle of Tannenberg 92,000 Russians surrendered.\n\n 
+        After marching through Belgium, Luxembourg and the Ardennes, the German Army advanced, 
+        in the latter half of August, into northern France where they met both the French army, 
+        under Joseph Joffre, and the initial six divisions of the British Expeditionary Force, 
+        under Sir John French. A series of engagements known as the Battle of the Frontiers ensued. 
+        Key battles included the Battle of Charleroi and the Battle of Mons. 
+        In the former battle the French 5th Army was almost destroyed by the German 2nd and 3rd 
+        Armies and the latter delayed the German advance by a day. A general Allied retreat followed, 
+        resulting in more clashes such as the Battle of Le Cateau, the Siege of Maubeuge and the Battle of St. Quentin (Guise). 
+        \n\nThe German army came within 70 km (43 mi) of Paris, but at the First Battle of the Marne (6–12 September), 
+        French and British troops were able to force a German retreat by exploiting a gap which appeared between the 1st and 2nd Armies, 
+        ending the German advance into France. The German army retreated north of the Aisne River and dug in there, establishing the beginnings 
+        of a static western front that was to last for the next three years. Following this German setback, the opposing forces tried to 
+        outflank each other in the Race for the Sea, and quickly extended their trench systems from the North Sea to the Swiss frontier. 
+        The resulting German-occupied territory held 64% of France's pig-iron production, 24% of its steel 
+        manufacturing, dealing a serious, but not crippling setback to French industry.\n"""
+        cls.text_too_short = "de"
+        cls.model_name = "en_core_web_sm"
 
-    def test_ef_client(self):
-        self.assertEqual(self.doc._.metadata['ok'], True)
-        self.assertEqual(self.doc._.metadata['status_code'], 200)
+    def test_normal_ef_client(self):
+        nlp = spacy.load(self.model_name)
+        nlp.add_pipe("entityfishing")
+        doc = nlp(self.text_example)
+        self.assertEqual(doc._.metadata["ok"], True)
+        self.assertEqual(doc._.metadata["status_code"], 200)
+        self.assertEqual(len(doc._.annotations["entities"]), 22)
 
-    def test_linking(self):
-        self.assertEqual(len(self.doc.ents), 77)
+    def test_bad_property_ef_client(self):
+        nlp = spacy.load(self.model_name)
+        nlp.add_pipe("entityfishing", config={"api_ef_base": "http://www.badurl.com"})
+        doc = nlp(self.text_example)
+        self.assertEqual(doc._.metadata["ok"], False)
+        self.assertEqual(doc._.metadata["status_code"], 404)
+        self.assertEqual(len(doc._.annotations), 0)
+
+    def test_bad_language_ef_client(self):
+        nlp = spacy.load(self.model_name)
+        nlp.add_pipe("entityfishing", config={"language": "def"})
+        doc = nlp(self.text_example)
+        self.assertEqual(doc._.metadata["ok"], False)
+        self.assertEqual(doc._.metadata["status_code"], 406)
+        self.assertEqual(doc._.annotations["message"], "The language specified is not supported or not valid. ")
+
+    def test_bad_text_ef_client(self):
+        nlp = spacy.load(self.model_name)
+        nlp.add_pipe("entityfishing")
+        doc = nlp(self.text_too_short)
+        self.assertEqual(doc._.metadata["ok"], False)
+        self.assertEqual(doc._.metadata["status_code"], 400)
+        self.assertEqual(doc._.annotations, {})
 
 
