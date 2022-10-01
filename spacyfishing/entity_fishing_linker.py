18dd2694289118e8fbd800a73ffa3ec6c0ef9efa
David Berenstein <david.m.berenstein@gmail.com>
2022-10-01 23:34:53 +0200

    added pipeline .pipe() batch support
    implemented asynchronous requests to facilitate batch support.

--- spacyfishing/entity_fishing_linker.py
+++ spacyfishing/entity_fishing_linker.py
@@ -6,12 +6,14 @@ SpaCy wrapper to call Entity-fishing API
 as disambiguation and entity linking component.
 """
 
-import logging
-from typing import Tuple
+import concurrent.futures
+import enum
 import json
+import logging
+from typing import List, Tuple
 
 import requests
-
+from spacy import util
 from spacy.language import Language
 from spacy.tokens import Doc, Span
 
@@ -88,11 +90,11 @@ class EntityFishing:
         Span.set_extension("other_ids", default=None, force=True)
 
     @staticmethod
-    def generic_client(method: str,
-                       url: str,
-                       verbose: bool,
-                       params: dict = None,
-                       files: dict = None) -> requests.Response:
+    def generic_client_batch(method: str,
+                             url_batch: List[str],
+                             verbose: bool,
+                             params: dict = None,
+                             files_batch: List[dict] = None) -> requests.Response:
         """Client to interact with a generic Rest API.
 
         Parameters:
@@ -108,41 +110,62 @@ class EntityFishing:
 
         if params is None:
             params = {}
-        if files is None:
-            files = {}
+        if files_batch is None:
+            files_batch = [{} for url in url_batch]
 
-        def make_requests(type_method: str,
-                          type_url: str,
-                          type_params: dict,
-                          type_files: dict) -> requests.Response:
-            res = requests.request(method=type_method,
-                                   url=type_url,
-                                   headers={
-                                       "Accept": "application/json"
-                                   },
-                                   files=type_files,
-                                   params=type_params)
-            return res
+        def load_url(type_url, type_files):
+            if method == "POST":
+                return requests.post(
+                    url=type_url,
+                    headers={
+                        "Accept": "application/json"
+                    },
+                    files=type_files,
+                    params=params)
+            else:
+                return requests.get(
+                    url=type_url,
+                    headers={
+                        "Accept": "application/json"
+                    },
+                    files=type_files,
+                    params=params)
+
+        response_batch = []
+        resp_err, resp_ok = 0, 0
+        with concurrent.futures.ThreadPoolExecutor(max_workers=20) as executor:
+            future_to_url = {executor.submit(
+                load_url, type_url, type_files): (type_url, type_files) for type_url, type_files in zip(url_batch, files_batch)}
+            for future in concurrent.futures.as_completed(future_to_url):
+                url = future_to_url[future]
+                try:
+                    response_batch.append(future.result())
+                except Exception as exc:
+                    resp_err = resp_err + 1
+                else:
+                    resp_ok = resp_ok + 1
 
         def client_log(msg: str) -> None:
             if verbose:
                 logging.warning(msg)
 
-        response = make_requests(method, url, params, files)
-
         # Manage response status code :
         # cf. https://nerd.readthedocs.io/en/latest/restAPI.html#response-status-codes
-        if response.status_code == 400:
-            client_log("Wrong request, missing parameters, "
-                       "missing header, text too short (<= 5 characters). (400)")
-        elif response.status_code == 500:
-            client_log("Entity-Fishing API service seems broken. (500)")
-        elif response.status_code == 404:
-            client_log("Property was not found in request body. (404)")
-        elif response.status_code == 406:
-            client_log("Language is not supported by Entity-Fishing. (406)")
+        for idx, response in enumerate(response_batch):
+            if response.status_code == 400:
+                client_log(f"Request {idx}. Wrong request, missing parameters, "
+                           "missing header, text too short (<= 5 characters). (400)")
+            elif response.status_code == 500:
+                client_log(
+                    f"Request {idx}. Entity-Fishing API service seems broken. (500)")
+            elif response.status_code == 404:
+                client_log(
+                    f"Request {idx}. Property was not found in request body. (404)")
+            elif response.status_code == 406:
+                client_log(
+                    f"Request {idx}. Language is not supported by Entity-Fishing. (406)")
 
-        return response
+        return response_batch
 
     @staticmethod
     def process_response(response: requests.models.Response) -> Tuple[dict, dict]:
@@ -196,8 +219,8 @@ class EntityFishing:
                 "entities": [
                     {
                         "rawName": ent.text,
-                        "offsetStart": ent.start_char,
-                        "offsetEnd": ent.end_char,
+                        "offsetStart": ent.start,
+                        "offsetEnd": ent.end,
                     } for ent in entities
                 ],
                 "mentions": [],
@@ -237,8 +260,7 @@ class EntityFishing:
                 pass
 
     # ~ Entity-fishing call service methods ~:
-
-    def concept_look_up(self, wiki_id: str) -> requests.Response:
+    def concept_look_up_batch(self, wiki_id_batch: str) -> requests.Response:
         """Service returns the knowledge base concept information from QID
         or Wikipedia page id.
 
@@ -247,13 +269,14 @@ class EntityFishing:
         Returns:
             response (requests.Response): query response from concept look-up service.
         """
-        url_concept_lookup = self.api_ef_base + "kb/concept/" + wiki_id
-        return self.generic_client(method="GET",
-                                   url=url_concept_lookup,
-                                   params=self.language,
-                                   verbose=self.verbose)
+        url_concept_lookup_batch = [
+            self.api_ef_base + "kb/concept/" + wiki_id for wiki_id in wiki_id_batch]
+        return self.generic_client_batch(method="GET",
+                                         url_batch=url_concept_lookup_batch,
+                                         params=self.language,
+                                         verbose=self.verbose)
 
-    def disambiguate_text(self, files: dict) -> requests.Response:
+    def disambiguate_text_batch(self, files_batch: List[dict]) -> requests.Response:
         """Service returns disambiguate entities.
 
         Parameters:
@@ -263,10 +286,11 @@ class EntityFishing:
             response (requests.Response): query response from concept look-up service.
         """
         url_disambiguate = self.api_ef_base + "disambiguate"
-        return self.generic_client(method='POST',
-                                   url=url_disambiguate,
-                                   files=files,
-                                   verbose=self.verbose)
+        url_disambiguate_batch = [url_disambiguate for file in files_batch]
+        return self.generic_client_batch(method='POST',
+                                         url_batch=url_disambiguate_batch,
+                                         files_batch=files_batch,
+                                         verbose=self.verbose)
 
     def look_extra_informations_on_entity(self, span: Span, res_desc: dict) -> None:
         """Attach to span extra information:
@@ -312,51 +336,28 @@ class EntityFishing:
         except requests.exceptions.JSONDecodeError:
             pass
 
-    def main_disambiguation_process(self,
-                                    text: str,
-                                    terms: str,
-                                    entities: list) -> Tuple[dict, dict, list]:
-        """Generic routine that describe the call
-        process of disambiguation service.
-        1. prepare query for disambiguation service
-        2. post query
-        3. process response
+    def main_disambiguation_process_batch(self,
+                                          text_batch: List[str],
+                                          terms_batch: List[str],
+                                          entities_batch: List[list]) -> List[Tuple[dict, dict, list]]:
+        data_to_post_batch = [self.prepare_data(text=text,
+                                                terms=terms,
+                                                entities=entities,
+                                                language=self.language,
+                                                full=self.flag_extra) for text, terms, entities in zip(text_batch, terms_batch, entities_batch)]
+        reqs = self.disambiguate_text_batch(files_batch=data_to_post_batch)
 
-        Parameters:
-            text (str): Text to disambiguate.
-            terms (str): Sequence of terms to disambiguate
-            e.g. "ONU Barack Obama president ...".
-            entities (list): Specific entities to disambiguate.
+        response_tuples = []
+        for req in reqs:
+            res, metadata = self.process_response(response=req)
+            try:
+                entities_enhanced = res['entities']
+            except KeyError:
+                entities_enhanced = []
+            response_tuples.append((res, metadata, entities_enhanced))
+        return response_tuples
 
-        Returns:
-            res (dict): response from Entity-Fishing service.
-            metadata (dict): information about HTTP request on
-            Entity-Fishing service.
-            entities_enhanced (list): list of entities disambiguated by
-            Entity-Fishing.
-        """
-
-        data_to_post = self.prepare_data(text=text,
-                                         terms=terms,
-                                         entities=entities,
-                                         language=self.language,
-                                         full=self.flag_extra)
-
-        req = self.disambiguate_text(files=data_to_post)
-        res, metadata = self.process_response(response=req)
-        try:
-            entities_enhanced = res['entities']
-        except KeyError:
-            entities_enhanced = []
-
-        return res, metadata, entities_enhanced
-
-    def __call__(self, doc: Doc) -> Doc:
-        """Attaches entities to spans (and doc)."""
-        # 1. Disambiguate and linking named entities in Doc object with Entity-Fishing
-        result_from_ef_text = self.main_disambiguation_process(text=doc.text,
-                                                               terms="",
-                                                               entities=doc.ents)
+    def process_single_doc_after_call(self, doc: Doc, result_from_ef_text) -> Doc:
         entities_from_text = result_from_ef_text[2]
 
         # 1a. Attach raw response (with text method in Entity-Fishing service) to doc
@@ -391,9 +392,11 @@ class EntityFishing:
         if len(nil_clustering) != 0:
             # prepare query for Entity-Fishing terms disambiguation
             terms = " ".join([ent.text for ent in doc.ents])
-            result_from_ef_terms = self.main_disambiguation_process(text="",
-                                                                    terms=terms,
-                                                                    entities=nil_clustering)
+            result_from_ef_terms = self.main_disambiguation_process_batch(
+                text_batch=[""],
+                terms_batch=[terms],
+                entities_batch=[nil_clustering]
+            )[0]
 
             entities_from_terms = result_from_ef_terms[2]
 
@@ -416,3 +419,33 @@ class EntityFishing:
                 pass
 
         return doc
+
+    def __call__(self, doc: Doc,) -> Doc:
+        """Attaches entities to spans (and doc)."""
+        # 1. Disambiguate and linking named entities in Doc object with Entity-Fishing
+        result_from_ef_text = self.main_disambiguation_process_batch(
+            text_batch=[doc.text],
+            terms_batch=[""],
+            entities_batch=[doc.ents]
+        )[0]
+        return self.process_single_doc_after_call(doc, result_from_ef_text)
+
+    def pipe(self, stream, batch_size=128):
+        """
+        It takes a stream of documents, and for each document,
+        it generates a list of sentence triplets,
+        and then sets the annotations for each sentence in the document
+        :param stream: a generator of Doc objects
+        :param batch_size: The number of documents to process at a time, defaults to 128 (optional)
+        """
+        for docs in util.minibatch(stream, size=batch_size):
+            text_batch = [doc.text for doc in docs]
+            entities_batch = [doc.ents for doc in docs]
+            terms_batch = ["" for _ in text_batch]
+
+            # 1. Disambiguate and linking named entities in Doc object with Entity-Fishing
+            result_from_ef_text_batch = self.main_disambiguation_process_batch(
+                text_batch=text_batch, terms_batch=terms_batch, entities_batch=entities_batch)
+
+            for doc, result_from_ef_text in zip(docs, result_from_ef_text_batch):
+                yield self.process_single_doc_after_call(doc, result_from_ef_text)
